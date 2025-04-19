b3bfa94871fcc81150d1a95f9bba4c31b49f4db5
Tiurne Viktoriya <tiurne@ukr.net>
2025-04-19 17:11:03 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-
 import { errorHandler } from "./error";
 import { connectDB } from "./db";
 import router from "./router";
