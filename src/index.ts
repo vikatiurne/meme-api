00bb7f8248d5b950c00b48216cf96b6d13531da7
Tiurne Viktoriya <tiurne@ukr.net>
2025-04-19 17:14:33 +0300

    cors origin

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: 'https://meme-web-production.up.railway.app',
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Autorization"],
   })
